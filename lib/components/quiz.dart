527b674abfd3dca0d08d3ddd870bcbb3ccc3cab1
mrbenamiller <mrbenamiller@gmail.com>
2025-04-10 12:18:17 +0000

    Need to be able to navigate through quiz questions

--- lib/components/quiz.dart
+++ lib/components/quiz.dart
@@ -10,8 +10,56 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  int _quizIndex = 0;
+
+  void _incrementQuizIndex() {
+    if (_quizIndex < widget.questions.length - 1) {
+      setState(() {
+        _quizIndex++;
+      });
+    }
+  }
+
+  void _decrementQuizIndex() {
+    if (_quizIndex > 0) {
+      setState(() {
+        _quizIndex--;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Quiz")));
+    return Stack(
+      children: [
+        Positioned(
+          top: 0,
+          child: Column(
+            children: [
+              Card(child: Text(widget.questions[_quizIndex].questionText)),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: _decrementQuizIndex,
+                  child: const Text("Previous Question"),
+                ),
+                ElevatedButton(
+                  onPressed: _incrementQuizIndex,
+                  child: const Text('Next Question'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
