b754d698fa8831b3d797fe00d31152a7bfce9dc1
mrbenamiller <mrbenamiller@gmail.com>
2025-04-10 13:45:24 +0000

    Adds some test questions

--- lib/screens/rush.dart
+++ lib/screens/rush.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/components/quiz.dart';
+import 'package:myapp/models/question.dart';
 
 class Rush extends StatefulWidget {
   const Rush({super.key, required this.title});
@@ -10,6 +12,24 @@ class Rush extends StatefulWidget {
 }
 
 class _RushState extends State<Rush> {
+  final List<QuizQuestion> questions = [
+    QuizQuestion(
+      questionText: "Question 1",
+      answerOptions: ["Option A", "Option B", "Option C"],
+      correctAnswer: "Option A",
+    ),
+    QuizQuestion(
+      questionText: "Question 2",
+      answerOptions: ["Option A", "Option B", "Option C"],
+      correctAnswer: "Option B",
+    ),
+    QuizQuestion(
+      questionText: "Question 3",
+      answerOptions: ["Option A", "Option B", "Option C"],
+      correctAnswer: "Option C",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +37,7 @@ class _RushState extends State<Rush> {
         title: Text(widget.title),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Text("Testing"),
+      body: Quiz(questions: questions),
     );
   }
 }
