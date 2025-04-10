f5ae2d39e9e9f98bd24c89d389d04c357b9a50d9
mrbenamiller <mrbenamiller@gmail.com>
2025-04-10 11:40:06 +0000

    WIP - Placeholder question model

---
+++ lib/models/question.dart
@@ -0,0 +1,11 @@
+class QuizQuestion {
+  final String questionText;
+  final List<String> answerOptions;
+  final String correctAnswer;
+
+  QuizQuestion({
+    required this.questionText,
+    required this.answerOptions,
+    required this.correctAnswer,
+  });
+}
