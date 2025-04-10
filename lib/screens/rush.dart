1ad7ba181c848ac81cff8b4932d95581743edff6
mrbenamiller <mrbenamiller@gmail.com>
2025-04-10 11:39:07 +0000

    Adds placeholder rush puzzle screen

---
+++ lib/screens/rush.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Rush extends StatefulWidget {
+  const Rush({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Rush> createState() => _RushState();
+}
+
+class _RushState extends State<Rush> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Text("Testing"),
+    );
+  }
+}
